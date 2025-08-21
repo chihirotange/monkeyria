9f678ac1cb7ca6cd4beb445ccc1939c977a55c28
Lam LV <zellitsss@gmail.com>
2025-08-21 23:34:11 +0700

    Refactor object interaction for better implementation

--- assets/scripts/ResourceGenerator.ts
+++ assets/scripts/ResourceGenerator.ts
@@ -1,29 +1,34 @@
 import { _decorator, CCFloat, CCInteger, Component, Node } from 'cc';
-import { InteractableObject } from './InteractableObject';
 import { ItemType } from './ItemType';
+import { PeriodInteractableObject } from './interactable/PeriodInteractableObject';
+import { Character } from './Character';
 const { ccclass, property, type } = _decorator;
 
 @ccclass('ResourceGenerator')
-export class ResourceGenerator extends InteractableObject {
+export class ResourceGenerator extends PeriodInteractableObject {
     @type(ItemType)
     itemType: ItemType = ItemType.None;
-
+    
     @type(CCInteger)
     maxAmount: number = 1;
-
+    
     @type(CCFloat)
     generatingInterval: number = 2; 
-
+    
     startGenerating() {
-
+        
     }
-
+    
     stopGenerating() {
-
+        
+    }
+    
+    generateResource() {
+        
     }
 
-    generateResource() {
-
+    processInterval(character: Character) {
+        // let character take resource
     }
 }
 
---
+++ assets/scripts/interactable/PeriodInteractableObject.ts
@@ -0,0 +1,32 @@
+import { _decorator, CCFloat, Component, Node } from 'cc';
+import { InteractableObject } from './InteractableObject';
+import { Character } from '../Character';
+const { ccclass, type, property } = _decorator;
+
+@ccclass('PeriodInteractableObject')
+export abstract class PeriodInteractableObject extends InteractableObject {
+
+    @type(CCFloat)
+    period: number = 1;
+
+    private _scheduledCallbacks: Map<Character, Function> = new Map();
+
+    beginInteraction(character: Character): void {
+        if (this._scheduledCallbacks.has(character)) return;
+        const callback = () => this.processInterval(character);
+        this._scheduledCallbacks.set(character, callback);
+        this.schedule(callback, this.period);
+    }
+
+    endInteraction(character: Character): void {
+        const callback = this._scheduledCallbacks.get(character);
+        if (callback) {
+            this.unschedule(callback);
+            this._scheduledCallbacks.delete(character);
+        }
+    }
+
+    abstract processInterval(character: Character);
+}
+
+
