a409e21707ae681ef76a43de33590d792fe53fcc
Lam LV <zellitsss@gmail.com>
2025-08-24 16:10:44 +0700

    Add crop asset

---
+++ assets/scripts/CropTile.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Crop')
+export class Crop extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
