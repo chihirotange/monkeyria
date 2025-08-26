ef0313ae89484c1b98ae77e4a705e543bdfe37a7
Lam Le <zellitsss@gmail.com>
2025-08-26 14:02:12 +0700

    Carry and flip

--- assets/scripts/CharacterAnimationController.ts
+++ assets/scripts/CharacterAnimationController.ts
@@ -1,5 +1,6 @@
 import { _decorator, Animation, AnimationClip, AnimationState, Component, Node, Vec3 } from 'cc';
 import { CharacterMovement } from './CharacterMovement';
+import { Character } from './Character';
 const { ccclass, property } = _decorator;
 
 @ccclass('CharacterAnimationController')
@@ -9,31 +10,38 @@ export class CharacterAnimationController extends Component {
     private _idleState: AnimationState = null;
     private _walkState: AnimationState = null;
     private _carryState: AnimationState = null;
+    private _character: Character = null;
 
     start() {
         this._animationComp = this.getComponent(Animation);
         this._idleState = this._animationComp.getState('player_idle');
         this._walkState = this._animationComp.getState('player_walk');
-        this._carryState = this._animationComp.getState('olayer_carry');
+        this._carryState = this._animationComp.getState('player_carry');
         this._movementComponent = this.node.parent.getComponent(CharacterMovement);
+        this._character = this.node.parent.getComponent(Character);
     }
 
     update(deltaTime: number) {
-        if (this._movementComponent) {
-            let direction = this._movementComponent.direction;
+        let direction = this._movementComponent.direction;
+        if (this._character.getInventory().getTotalResourceAmount() > 0) {
+            this.playState(this._carryState);
+        }
+        else {
             let magSqr = direction.lengthSqr();
-            let currentScale = this.node.getScale();
-            let scaleX = currentScale.x;
             if (magSqr > 0) {
                 this.playState(this._walkState);
-                scaleX = Math.abs(scaleX);
-                scaleX *= direction.x < 0 ? -1 : 1;
             }
             else {
                 this.playState(this._idleState);
             }
-            this.node.setScale(new Vec3(scaleX, currentScale.y, currentScale.z));
         }
+
+        // flip
+        let currentScale = this.node.getScale();
+        let scaleX = currentScale.x;
+        scaleX = Math.abs(scaleX);
+        scaleX *= direction.x < 0 ? -1 : 1;
+        this.node.setScale(new Vec3(scaleX, currentScale.y, currentScale.z));
     }
 
     playState(state: AnimationState) {
@@ -43,5 +51,3 @@ export class CharacterAnimationController extends Component {
         }
     }
 }
-
-
