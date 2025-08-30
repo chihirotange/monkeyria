327093c75b5a1e0cf829d2ace9c5897dfc0bcb36
Lam LV <zellitsss@gmail.com>
2025-08-30 23:43:35 +0700

    Add after save script for copying files

---
+++ assets/maps/after-save.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Copy Terrain_top.png
+copy /Y ".\monkeyria\simplified\Level_0\Terrain_top.png" "..\resources\Terrain_top.png"
+REM Copy Terrain.png
+copy /Y ".\monkeyria\simplified\Level_0\Terrain.png" "..\resources\Terrain.png"
+REM Copy data.json
+copy /Y ".\monkeyria\simplified\Level_0\data.json" "..\resources\data.json"
