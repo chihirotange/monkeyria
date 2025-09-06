dd941170ec5b9ef0ecf9f8c618ec00a0a0e05748
Lam LV <zellitsss@gmail.com>
2025-09-06 22:49:02 +0700

    Able to load entities from ldtk

--- assets/scripts/MapManager.ts
+++ assets/scripts/MapManager.ts
@@ -1,6 +1,15 @@
-import { _decorator, Asset, assetManager, Component, ImageAsset, JsonAsset, Node, resources, Sprite, SpriteFrame } from 'cc';
+import { _decorator, Asset, assetManager, CCFloat, CCInteger, CCString, Component, ImageAsset, instantiate, JsonAsset, Node, Prefab, resources, Sprite, SpriteFrame, Vec2, Vec3 } from 'cc';
 import { parseLdtkProject } from './ldtkType';
 const { ccclass, property, type } = _decorator;
+
+@ccclass('PrefabMappingDef')
+export class PrefabMappingDef {
+    @property
+    type: string = '';
+
+    @type(Prefab)
+    prefab: Prefab = null;
+}
 
 @ccclass('MapManager')
 export class MapManager extends Component {
@@ -8,6 +17,17 @@ export class MapManager extends Component {
     BG: Sprite = null;
     @type(Sprite)
     BG_top: Sprite = null;
+
+    @type(Node)
+    gameObjectsLayer: Node = null;
+
+    @type(CCFloat)
+    spriteScaleFactor: number = 4;
+
+    @property({
+        type: [PrefabMappingDef]
+    })
+    prefabMappingDefs: PrefabMappingDef[] = [];
 
     start() {
         // assetManager.loadBundle('resources', (loadBundleError, bundle) => {
@@ -20,6 +40,12 @@ export class MapManager extends Component {
                 return;
             }
             this.BG.spriteFrame = data;
+            resources.load('monkeyria', Asset, (err, asset) => {
+            if (err) {
+                return;
+            }
+            this.onMapDataLoaded(asset.nativeAsset);
+        });
         });
         resources.load('Terrain_top/spriteFrame', SpriteFrame, (err, data) => {
             if (err) {
@@ -27,15 +53,46 @@ export class MapManager extends Component {
             }
             this.BG_top.spriteFrame = data;
         });
-        resources.load('monkeyria', Asset, (err, asset) => {
-            if (err) {
-                return;
-            }
-            this.onMapDataLoaded(asset.nativeAsset);
-        });
+        
     }
     onMapDataLoaded(jsonString: string) {
         let ldtk = parseLdtkProject(jsonString);
-        console.log(ldtk.defs);
+
+        let mapAnchor = new Vec3();
+        mapAnchor = mapAnchor.add(this.BG.node.getWorldPosition());
+        const size = this.BG.spriteFrame.originalSize;
+        const scale = this.BG.node.getWorldScale();
+        const width = size.width * scale.x;
+        const height = size.height * scale.y;
+        mapAnchor = mapAnchor.add(new Vec3(-width / 2, height / 2, 0));
+
+        if (ldtk.levels.length > 0) {
+            let level = ldtk.levels[0];
+            let entitiesLayer = level.layerInstances.find((layer) => {
+                return layer.__type === 'Entities';
+            });
+            if (entitiesLayer) {
+                entitiesLayer.entityInstances.forEach((entityInstance) => {
+                    let entityType = entityInstance.fieldInstances.find((field) => {
+                        return field.__identifier === 'EntityType';
+                    });
+
+                    if (entityType != undefined) {
+                        let foundDef = this.prefabMappingDefs.find((def) => {
+                            return def.type === entityType.__value;
+                        });
+                        if (foundDef) {
+                            let entity = instantiate(foundDef.prefab);
+                            entity.parent = this.gameObjectsLayer;
+                            // get top-left position of this.BG
+                            entity.setWorldPosition(new Vec3(entityInstance.__worldX * this.spriteScaleFactor, entityInstance.__worldY * this.spriteScaleFactor * -1, 0).add(mapAnchor));
+                        }
+                        else {
+                            console.log('No mapping for type ' + entityInstance.__identifier);
+                        }
+                    }
+                });
+            }
+        }
     }
 }
